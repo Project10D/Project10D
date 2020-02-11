4ed83e6e0e43815c38feffc62bdeb80f64897a43
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2020-02-11 23:05:14 +0300

    MainMenu
    сделал главное меню (только кнопка "начать игру", загрузка тормозит).

---
+++ MagnetGun/Assets/Scripts/Logic/NewLevelButton_Logic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewLevelButton_Logic : MonoBehaviour {
+	public string level;
+	public Element_Logic logic;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	void FixedUpdate(){
+		if (logic.value) {
+			SceneManager.LoadScene(level, LoadSceneMode.Single);
+		}
+	}
+}
+
---
+++ MagnetGun/Assets/Scripts/OnObjClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnObjClick : MonoBehaviour {
+
+	public GameObject definedButton;
+	public Element_Logic logic;
+	// Use this for initialization
+	void Start () {
+		definedButton = this.gameObject;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit Hit;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+			{
+				Debug.Log("Button Clicked");
+				logic.value = true;
+			}
+		}    
+	}
+}
